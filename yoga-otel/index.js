6711e66028adf76af616c43667986272a909a677
danstarns <danielstarns@hotmail.com>
2024-04-29 20:28:24 +0700

    feat: add yoga otel example

---
+++ yoga-otel/index.js
@@ -0,0 +1,57 @@
+import cluster from "cluster";
+import { cpus } from "os";
+import { createSchema, createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+import opentelemetry from "@opentelemetry/api";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import {
+  BasicTracerProvider,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+
+const provider = new BasicTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "basic-service",
+  }),
+});
+
+const exporter = new OTLPTraceExporter({});
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+provider.register();
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < cpus().length; i++) {
+    cluster.fork();
+  }
+} else {
+  const yoga = createYoga({
+    schema: createSchema({
+      typeDefs: `
+        type Query {
+          hello: String!
+        }
+      `,
+      resolvers: {
+        Query: {
+          hello: () => {
+            const tracer = opentelemetry.trace.getTracer("example-tracer");
+            const span = tracer.startSpan("say-hello");
+            span.setAttribute("hello-to", "world");
+            span.addEvent("invoking resolvers");
+            span.end();
+            return "world";
+          },
+        },
+      },
+    }),
+    logging: false,
+  });
+
+  const server = createServer(yoga);
+
+  server.listen(8000);
+}
